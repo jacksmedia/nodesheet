687e2dcffed55704275fbd0f1c89793017360ac7
Jacks Media <alex@j4cks.com>
2020-11-12 20:12:37 -0800

    🛠 working on express callback to sheets
    v3 seems deprecated

--- app.js
+++ app.js
@@ -7,10 +7,10 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
-var fs              = require('fs'),
-var readline        = require('readline'),
-var {google}        = require('googleapis'),
-var request         = require('request'),
+var fs              = require('fs');
+var readline        = require('readline');
+var {google}        = require('googleapis');
+var request         = require('request');
 
 var app = express();
 
@@ -26,6 +26,26 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+// retrieve google sheet data
+app.get('/v3', function(req, res){
+	// my URL, no more options att
+	var options = {
+		url: 'https://spreadsheets.google.com/feeds/cells/2PACX-1vQgB1MdWL3Cd3X6hgp4xLLIEZTh7YpbCTQSrXuUuFuLJ1DPPtrwyu-JaTfuisHq2xy2v4jhjBKsCP1_/1/public/values?alt=json'
+	}
+	// give request package the sheet options
+	request(options, callback)
+	// callback function makes request, writes to console
+	function callback(error, response, body){
+		if (!error && response.statusCode == 200){
+			console.log(body);
+			// renders test template w response object
+			res.render('test', {response: response})
+		} else {
+			console.log(error);
+		}
+	}
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
